6fc22c59930ac3426237b5a69113c02c3904d969
bifeoluwa12 <igwt3030@gmail.com>
2025-06-19 13:02:55 +0100

    Add files via upload

---
+++ class1.js
@@ -0,0 +1,138 @@
+// let name = "Ifeoluwa";
+
+// console.log(name);
+// let age = "17";
+
+// console.log(age);
+// let state = "osun";
+
+// console.log(state);
+// let address = "ibadan";
+
+// console.log(address);
+// let religion = "christian";
+
+// console.log(religion);
+
+// const username = "ifeoluwa";
+
+// console.log(username);
+// const email = "igwt"
+
+// console.log(email);
+// const password = "adsssd123@";
+
+// console.log(password);
+
+// let sum = 0;
+
+// for (let i = 1; i <= 10; i++) {
+//   if (i % 2 !== 0) {
+//     sum = sum + i;
+//   }
+// }
+// console.log(sum);
+
+// while (0 < 5) {
+//   console.log("hello world");
+// }
+// let a=2;
+// while(a<5){
+//     console.log("hey there");
+//     a++
+// }
+// let a = 2;
+// let product = 1;
+// while (a <= 20) {
+//   if (a % 2 === 0) {
+//     product = product * a;
+//   }
+//   a++;
+// }
+// console.log("the product is", product);
+// let day = 0;
+
+// switch (day){
+//     case 0:
+//         console.log("sunday");
+//         break;
+//         case 1:
+//         console.log("monday");
+//         break;
+//         case 2:
+//         console.log("tuesday");
+//         break;
+//         case 3:
+//         console.log("wednesday");
+//         break;
+//         case 4:
+//         console.log("thursday");
+//         break;
+//         case 5:
+//         console.log("friday");
+//         break;
+//         default:
+//             console.log("invalid")
+// }
+// let day = 0;
+
+// switch (day) {
+//   case 0:
+//     console.log("sunday");
+//   case 1:
+//     console.log("monday");
+//   case 2:
+//     console.log("tuesday");
+//   case 3:
+//     console.log("wednesday");
+//   case 4:
+//     console.log("thursday");
+//   case 5:
+//     console.log("friday");
+//   default:
+//     console.log("invalid");
+// }
+// function greeting (username){
+//     console.log("hello", username)
+// }
+// greeting("promise")
+
+// function sum(x, y) {
+//   console.log("sum:", x + y);
+// }
+// sum(2, 3);
+
+// function calc(a, b, op) {
+//   if (op === "sum") {
+//     console.log("sum is:", a + b);
+//   } else if (op === "subs") {
+//     console.log("sub is:", a - b);
+//   } else if (op === "mult") {
+//     console.log("mult is:", a * b);
+//   } else if (op === "div") {
+//     console.log("div is:", a / b);
+//   } else {
+//     console.log("invalid operation");
+//   }
+// }
+// calc(2, 4, "sum");
+
+// a = Number(prompt("Enter the value a"));
+// b = Number(prompt("Enter the value b"));
+// c = prompt("Enter the operation");
+// function calc(x, y, op) {
+//   if (op === "sum") {
+//     return x + y;
+//   } else if (op === "subs") {
+//     return x - y;
+//   } else if (op === "mult") {
+//     return x * y;
+//   } else if (op === "div") {
+//     return x / y;
+//   } else {
+//     console.log("invalid operation");
+//   }
+// }
+// console.log(calc(a, b, c));
+
+
